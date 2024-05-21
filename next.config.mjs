65e5e61d021798464ffcf118fe73c8dcb9f8cc4f
Siddharth <siddharth6527@gmail.com>
2024-05-21 19:35:13 +0530

    updated config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ["https://vote-count-management-system-ui.onrender.com"],
+  },
   output: "export",
   eslint: {
     ignoreDuringBuilds: true,
